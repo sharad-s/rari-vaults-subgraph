ce9823cec2c2b847a9e0fdaa31538dfa6f45bd4a
sharad-s <sharadshekar@gmail.com>
2021-12-06 01:13:13 -0600

    logs

--- src/mappings/vault.ts
+++ src/mappings/vault.ts
@@ -1,8 +1,11 @@
 import { Initialized } from "../../generated/VaultFactory/Vault";
 import { Vault } from "../../generated/schema";
+import { log } from "@graphprotocol/graph-ts";
 
 export function handleVaultInitialized(event: Initialized): void {
   let vaultId = event.address;
+
+  log.info("🏁 Vault Initalized at address {}", [vaultId.toHexString()]);
 
   // const context = new DataSourceContext();
   let vault = Vault.load(vaultId.toHexString());
